3e0f1d0ca3853833549dcfeda7256edab93831bc
DUKT Squad - SHYNZ Development Team <duket@shynz.com>
2023-07-23 13:56:56 -0400

    Updated Links

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
                 </Link>
                 <Link
                   className={styles.secondaryCta}
-                  href="https://github.com/thirdweb-example/marketplace-v3"
+                  href="https://taupe-tanuki-791e20.netlify.app/"
                   target="_blank"
                 >
                   Visit Farmville
